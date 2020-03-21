d5ae29c9ef68d49c020222a9b364594507547b07
Jonathan Kemp <kempdogg@gmail.com>
2020-03-20 23:20:13 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,62 @@
+# map-plus [![Build Status](https://travis-ci.com/jonkemp/map-plus.svg?branch=master)](https://travis-ci.com/jonkemp/map-plus)
+
+> Produces a new array of values by mapping each value in list through a transformation function (iteratee).
+
+ The iteratee is passed three arguments: the `value`, then the index (or `key`) of the iteration, and finally a reference to the entire `list`. 
+
+Inspired by `_.map`. 😄
+
+
+## Install
+
+Install with [npm](https://npmjs.org/package/map-plus)
+
+```
+$ npm install map-plus
+```
+
+Or [unpkg](https://unpkg.com/map-plus/)
+
+```
+<script src="https://unpkg.com/map-plus@1.0.0/umd/index.js" />
+```
+
+Check out the unit tests on [CodePen](https://codepen.io/jonkemp/full/YzPBmwz).
+
+## Usage
+
+```js
+const map = require('map-plus');
+
+map([1, 2, 3], num => num * 3);
+//=> [3, 6, 9]
+
+map({one: 1, two: 2, three: 3}, (num, key) => num * 3);
+//=> [3, 6, 9]
+```
+
+---
+| **Like us a lot?** Help others know why you like us! **Review this package on [pkgreview.dev](https://pkgreview.dev/npm/map-plus)** | ➡   | [![Review us on pkgreview.dev](https://i.ibb.co/McjVMfb/pkgreview-dev.jpg)](https://pkgreview.dev/npm/map-plus) |
+| ----------------------------------------------------------------------------------------------------------------------------------------- | --- | --------------------------------------------------------------------------------------------------------------------- |
+
+## API
+
+### map(list, iteratee, [context])
+
+#### list
+
+Type: `array` or `object`  
+Default: `none`
+
+The collection of elements to iterate over.
+
+#### iteratee
+
+Type: `function` or `string`  
+Default: `none`
+
+Function to apply. Or property name to pass.
+
+## License
+
+MIT
---
+++ test.js
@@ -0,0 +1,53 @@
+const assert = require('assert');
+const map = require('./');
+const noop = () => {};
+const initial = (array, n, guard) => Array.prototype.slice.call(array, 0, Math.max(0, array.length - (n == null || guard ? 1 : n)));
+const first = (array, n, guard) => {
+	if (array == null || array.length < 1) return n == null ? void 0 : [];
+	if (n == null || guard) return array[0];
+
+	return initial(array, array.length - n);
+};
+
+describe('map', () => {
+	it('should produce a new array of values by mapping each value in list through an iteratee', () => {
+		assert.deepEqual(map([1, 2, 3], num => num * 3), [3, 6, 9]);
+		assert.deepEqual(map({one: 1, two: 2, three: 3}, num => num * 3), [3, 6, 9]);
+		assert.deepEqual(map([[1, 2], [3, 4]], first), [1, 3]);
+	});
+
+	it('should return doubled numbers', () => {
+		const doubled = map([1, 2, 3], num => num * 2);
+
+		assert.deepEqual(doubled, [2, 4, 6]);
+	});
+
+	it('should return tripled numbers with context', () => {
+		const tripled = map([1, 2, 3], function(num){ return num * this.multiplier; }, {multiplier: 3});
+
+		assert.deepEqual(tripled, [3, 6, 9]);
+	});
+
+	it('can use collection methods on Array-likes', () => {
+		const ids = map({length: 2, 0: {id: '1'}, 1: {id: '2'}}, ({id}) => id);
+
+		assert.deepEqual(ids, ['1', '2']);
+	});
+
+	it('should handle a null properly', () => {
+		assert.deepEqual(map(null, noop), []);
+	});
+
+	it('should call with proper context', () => {
+		assert.deepEqual(map([1], function() {
+			return this.length;
+		}, [5]), [1]);
+	});
+
+	it('should map predicate string to object properties', () => {
+		// Passing a property name like _.pluck.
+		const people = [{name: 'moe', age: 30}, {name: 'curly', age: 50}];
+
+		assert.deepEqual(map(people, 'name'), ['moe', 'curly']);
+	});
+});
