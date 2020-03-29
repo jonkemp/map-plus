7377099878a9bdb3c425e2029807d4470e7c573d
Jonathan Kemp <kempdogg@gmail.com>
2020-03-29 17:06:42 -0400

    Remove unused code to reduce bundle size.

--- index.js
+++ index.js
@@ -1,34 +1,44 @@
-const shallowProperty = key => obj => obj == null ? void 0 : obj[key];
-
-const MAX_ARRAY_INDEX = 2 ** 53 - 1;
-
-const getLength = shallowProperty('length');
-
-const isArrayLike = (collection) => {
-	const length = getLength(collection);
-
-	return typeof length == 'number' && length >= 0 && length <= MAX_ARRAY_INDEX;
-};
-
-const isFunction = obj => toString.call(obj) === '[object Function]';
-
-const isObject = obj => {
+var isObject = obj => {
 	const type = typeof obj;
 
 	return type === 'function' || type === 'object' && !!obj;
 };
 
-const isArguments = obj => toString.call(obj) === '[object Arguments]';
-
-const identity = value => value;
-
-const getKeys = (obj) => {
+var getKeys = (obj) => {
 	if (!isObject(obj)) return [];
 
 	return Object.keys(obj);
 };
 
-const isMatch = (object, attrs) => {
+var shallowProperty = key => obj => obj == null ? void 0 : obj[key];
+
+var getLength = shallowProperty('length');
+
+const MAX_ARRAY_INDEX = 2 ** 53 - 1;
+
+var isArrayLike = (collection) => {
+	const length = getLength(collection);
+
+	return typeof length == 'number' && length >= 0 && length <= MAX_ARRAY_INDEX;
+};
+
+var identity = value => value;
+
+var isFunction = obj => toString.call(obj) === '[object Function]';
+
+var optimizeCb = (func, context, argCount) => {
+	if (context === void 0) return func;
+	switch (argCount == null ? 3 : argCount) {
+		case 1: return value => func.call(context, value);
+			// The 2-argument case is omitted because we’re not using it.
+		case 3: return (value, index, collection) => func.call(context, value, index, collection);
+		case 4: return (accumulator, value, index, collection) => func.call(context, accumulator, value, index, collection);
+	}
+
+	return (...args) => func.apply(context, args);
+};
+
+var isMatch = (object, attrs) => {
 	const keys = getKeys(attrs);
 	const {length} = keys;
 
@@ -44,7 +54,7 @@ const isMatch = (object, attrs) => {
 	return true;
 };
 
-const matcher = attrs => {
+var matcher = attrs => {
 	attrs = Object.assign({}, attrs);
 
 	return obj => isMatch(obj, attrs);
@@ -61,24 +71,12 @@ const deepGet = (obj, path) => {
 	return length ? obj : void 0;
 };
 
-const property = path => {
+var property = path => {
 	if (!Array.isArray(path)) {
 		return shallowProperty(path);
 	}
 
 	return obj => deepGet(obj, path);
-};
-
-const optimizeCb = (func, context, argCount) => {
-	if (context === void 0) return func;
-	switch (argCount == null ? 3 : argCount) {
-		case 1: return value => func.call(context, value);
-			// The 2-argument case is omitted because we’re not using it.
-		case 3: return (value, index, collection) => func.call(context, value, index, collection);
-		case 4: return (accumulator, value, index, collection) => func.call(context, accumulator, value, index, collection);
-	}
-
-	return (...args) => func.apply(context, args);
 };
 
 const baseIteratee = (value, context, argCount) => {
@@ -93,33 +91,15 @@ let iteratee;
 
 const exportIteratee = iteratee = (value, context) => baseIteratee(value, context, Infinity);
 
-const cb = (value, context, argCount) => {
+var cb = (value, context, argCount) => {
 	if (iteratee !== exportIteratee) return iteratee(value, context);
 
 	return baseIteratee(value, context, argCount);
 };
 
-var lib = {
-	shallowProperty,
-	getLength,
-	isArrayLike,
-	isFunction,
-	isObject,
-	isArguments,
-	identity,
-	getKeys,
-	property,
-	matcher,
-	isMatch,
-	optimizeCb,
-	cb
-};
-
-const { getKeys: getKeys$1, isArrayLike: isArrayLike$1, cb: cb$1 } = lib;
-
 var map = (obj, iteratee, context) => {
-	iteratee = cb$1(iteratee, context);
-	const keys = !isArrayLike$1(obj) && getKeys$1(obj);
+	iteratee = cb(iteratee, context);
+	const keys = !isArrayLike(obj) && getKeys(obj);
 	const { length } = keys || obj;
 	const results = Array(length);
 
